53966daea05a983425bab560ae9c910ae19970a2
Frederick <125679913+FredMT@users.noreply.github.com>
2024-08-26 13:22:12 +0200

    console log google callback error

--- app/api/login/google/callback/route.ts
+++ app/api/login/google/callback/route.ts
@@ -1,17 +1,17 @@
-import { cookies } from "next/headers";
-import { OAuth2RequestError } from "arctic";
-import { googleAuth } from "@/lib/auth";
-import { createGoogleUserUseCase } from "@/use-cases/users";
-import { getAccountByGoogleIdUseCase } from "@/use-cases/accounts";
-import { afterLoginUrl } from "@/app-config";
-import { setSession } from "@/lib/session";
+import { cookies } from 'next/headers'
+import { OAuth2RequestError } from 'arctic'
+import { googleAuth } from '@/lib/auth'
+import { createGoogleUserUseCase } from '@/use-cases/users'
+import { getAccountByGoogleIdUseCase } from '@/use-cases/accounts'
+import { afterLoginUrl } from '@/app-config'
+import { setSession } from '@/lib/session'
 
 export async function GET(request: Request): Promise<Response> {
-  const url = new URL(request.url);
-  const code = url.searchParams.get("code");
-  const state = url.searchParams.get("state");
-  const storedState = cookies().get("google_oauth_state")?.value ?? null;
-  const codeVerifier = cookies().get("google_code_verifier")?.value ?? null;
+  const url = new URL(request.url)
+  const code = url.searchParams.get('code')
+  const state = url.searchParams.get('state')
+  const storedState = cookies().get('google_oauth_state')?.value ?? null
+  const codeVerifier = cookies().get('google_code_verifier')?.value ?? null
 
   if (
     !code ||
@@ -22,65 +22,66 @@ export async function GET(request: Request): Promise<Response> {
   ) {
     return new Response(null, {
       status: 400,
-    });
+    })
   }
 
   try {
     const tokens = await googleAuth.validateAuthorizationCode(
       code,
-      codeVerifier,
-    );
+      codeVerifier
+    )
     const response = await fetch(
-      "https://openidconnect.googleapis.com/v1/userinfo",
+      'https://openidconnect.googleapis.com/v1/userinfo',
       {
         headers: {
           Authorization: `Bearer ${tokens.accessToken}`,
         },
-      },
-    );
-    const googleUser: GoogleUser = await response.json();
+      }
+    )
+    const googleUser: GoogleUser = await response.json()
 
-    const existingAccount = await getAccountByGoogleIdUseCase(googleUser.sub);
+    const existingAccount = await getAccountByGoogleIdUseCase(googleUser.sub)
 
     if (existingAccount) {
-      await setSession(existingAccount.user_id);
+      await setSession(existingAccount.user_id)
       return new Response(null, {
         status: 302,
         headers: {
           Location: afterLoginUrl,
         },
-      });
+      })
     }
 
-    const userId = await createGoogleUserUseCase(googleUser);
-    await setSession(userId);
+    const userId = await createGoogleUserUseCase(googleUser)
+    await setSession(userId)
     return new Response(null, {
       status: 302,
       headers: {
         Location: afterLoginUrl,
       },
-    });
+    })
   } catch (e) {
+    console.log(e)
     // the specific error message depends on the provider
     if (e instanceof OAuth2RequestError) {
       // invalid code
       return new Response(null, {
         status: 400,
-      });
+      })
     }
     return new Response(null, {
       status: 500,
-    });
+    })
   }
 }
 
 export interface GoogleUser {
-  sub: string;
-  name: string;
-  given_name: string;
-  family_name: string;
-  picture: string;
-  email: string;
-  email_verified: boolean;
-  locale: string;
+  sub: string
+  name: string
+  given_name: string
+  family_name: string
+  picture: string
+  email: string
+  email_verified: boolean
+  locale: string
 }
