228a0dd08499ec971286bb0fa0e6bcdddad106a4
Frederick <125679913+FredMT@users.noreply.github.com>
2024-08-26 13:04:04 +0200

    console log sign up error for Vercel

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client'
 
-import { z } from "zod";
+import { z } from 'zod'
 
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 import {
   Form,
   FormControl,
@@ -12,15 +12,15 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { pageTitleStyles } from "@/styles/common";
-import { cn } from "@/lib/utils";
-import { useServerAction } from "zsa-react";
-import { signUpAction } from "./actions";
-import { LoaderButton } from "@/components/loader-button";
-import { useToast } from "@/components/ui/use-toast";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Terminal } from "lucide-react";
+} from '@/components/ui/form'
+import { pageTitleStyles } from '@/styles/common'
+import { cn } from '@/lib/utils'
+import { useServerAction } from 'zsa-react'
+import { signUpAction } from './actions'
+import { LoaderButton } from '@/components/loader-button'
+import { useToast } from '@/components/ui/use-toast'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Terminal } from 'lucide-react'
 
 const registrationSchema = z
   .object({
@@ -30,38 +30,39 @@ const registrationSchema = z
   })
   .refine((data) => data.password === data.passwordConfirmation, {
     message: "Passwords don't match",
-    path: ["passwordConfirmation"],
-  });
+    path: ['passwordConfirmation'],
+  })
 
 export default function RegisterPage() {
-  const { toast } = useToast();
+  const { toast } = useToast()
 
   const { execute, isPending, error } = useServerAction(signUpAction, {
     onError({ err }) {
+      console.log(err)
       toast({
-        title: "Something went wrong",
+        title: 'Something went wrong',
         description: err.message,
-        variant: "destructive",
-      });
+        variant: 'destructive',
+      })
     },
-  });
+  })
 
   const form = useForm<z.infer<typeof registrationSchema>>({
     resolver: zodResolver(registrationSchema),
     defaultValues: {
-      email: "",
-      password: "",
-      passwordConfirmation: "",
+      email: '',
+      password: '',
+      passwordConfirmation: '',
     },
-  });
+  })
 
   function onSubmit(values: z.infer<typeof registrationSchema>) {
-    execute(values);
+    execute(values)
   }
 
   return (
     <div className="mx-auto max-w-[400px] space-y-6 py-24">
-      <h1 className={cn(pageTitleStyles, "text-center")}>Sign Up</h1>
+      <h1 className={cn(pageTitleStyles, 'text-center')}>Sign Up</h1>
 
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
@@ -136,5 +137,5 @@ export default function RegisterPage() {
         </form>
       </Form>
     </div>
-  );
+  )
 }
